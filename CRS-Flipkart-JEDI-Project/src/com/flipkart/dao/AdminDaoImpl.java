7bce5ad0d098228d756fa0e8d736e5252771b9fc
Ashutosh-Kaushik <icm2017002@iiita.ac.in>
2021-05-26 10:38:41 +0530

    admin and professor dao implemented.

--- CRS-Flipkart-JEDI-Project/src/com/flipkart/dao/AdminDaoImpl.java
+++ CRS-Flipkart-JEDI-Project/src/com/flipkart/dao/AdminDaoImpl.java
@@ -6,9 +6,11 @@ import com.flipkart.bean.Professor;
 import com.flipkart.utils.DBUtil;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AdminDaoImpl implements AdminDaoInterface{
     @Override
@@ -85,4 +87,24 @@ public class AdminDaoImpl implements AdminDaoInterface{
         }
         return grades;
     }
+
+    @Override
+    public void modifyCourseDetails(Course course) {
+        try {
+            Connection con = DBUtil.getConnection();
+            String sql = "update Course set courseName = ?, department = ?, professorId = ?, semester = ?, courseCode = ? where courseId = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, course.getCourseName());
+            stmt.setString(2,course.getDepartment());
+            stmt.setInt(3, course.getProfessorId());
+            stmt.setInt(4, course.getSemester());
+            stmt.setString(5, course.getCourseCode());
+            stmt.setInt(6, course.getCourseId());
+            stmt.executeUpdate();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
