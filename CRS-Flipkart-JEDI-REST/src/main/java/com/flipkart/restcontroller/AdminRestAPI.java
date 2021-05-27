01741162b0a67f694d7d3533fadd1f0d0d4ec93b
Dhruv-Adlakha <dhruvadl25@gmail.com>
2021-05-27 18:56:04 +0530

    Added more files to the REST project structure

---
+++ CRS-Flipkart-JEDI-REST/src/main/java/com/flipkart/restcontroller/AdminRestAPI.java
@@ -0,0 +1,5 @@
+package com.flipkart.restcontroller;
+
+public class AdminRestAPI {
+
+}
--- CRS-Flipkart-JEDI-REST/src/main/java/com/flipkart/restcontroller/ApplicationConfig.java
+++ CRS-Flipkart-JEDI-REST/src/main/java/com/flipkart/restcontroller/ApplicationConfig.java
@@ -5,7 +5,10 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class ApplicationConfig extends ResourceConfig {
 
 	public ApplicationConfig() {
-		//register(CustomerRestApi.class);
+		register(StudentRestAPI.class);
+		register(UserRestAPI.class);
+		register(ProfessorRestAPI.class);
+		register(AdminRestAPI.class);
 	}
 
 }
